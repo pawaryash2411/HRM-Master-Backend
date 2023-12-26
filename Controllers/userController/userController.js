5928329900d17c2f94afd909da5a9b731a40cda4
Rehan-Multani <135141111+Rehan-Multani@users.noreply.github.com>
2023-12-26 19:22:27 +0530

    rgg
    rgrg

--- Controllers/userController/userController.js
+++ Controllers/userController/userController.js
@@ -1,100 +1,83 @@
 const db = require("../../Models/userModel/userModel");
 const validator = require('validator');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const sendEmail = require('../../Controllers/emailController');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+    const { firstname, lastname, email, password, gender, contact } = req.body;
 
-  try {
-  //   if (!name || !email || !password) {
-  //     return res.status(400).json({ message: "Please enter all fields" });
-  //   }
+    try {
+        // Check if all required fields are present and not empty
+        if (!firstname || !lastname || !email || !password || !gender || !contact) {
+            return res.status(400).json({ message: "Please enter all fields" });
+        }
 
-    if (!validator.isEmail(email)) {
-      return res.status(400).json({ message: "Please enter a valid email" });
+        // Additional validations for email and password
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ message: "Please enter a valid email" });
+        }
+
+        if (!validator.isStrongPassword(password)) {
+            return res.status(400).json({ message: "Please enter a strong password" });
+        }
+
+        const exists = await db.findOne({ email });
+        if (exists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new db({
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword,
+            gender,
+            contact,
+        });
+
+        const userRegister = await newUser.save();
+        res.status(200).json({ userRegister });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-
-    if (!validator.isStrongPassword(password)) {
-      return res.status(400).json({ message: "Please enter a strong password" });
-    }
-
-    const exists = await db.findOne({ email });
-    if (exists) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const newUser = new db({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    const userRegister = await newUser.save();
-    res.status(200).json({ userRegister });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1yr",
-  });
-};
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: "1yr",
+    });
+  };
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    if (!email || !password) {
-      return res.status(400).json({ message: "Please enter all fields" });
+    // console.log("data", req.body);
+    const { email, password } = req.body;
+    try {
+      if (!email || !password) {
+        return res.status(400).json({ message: "Please enter all fields" });
+      }
+      const user = await db.findOne({ email });
+      console.log(user);
+      if (!user) {
+        return res.status(400).json({ message: "User does not exist" });
+      }
+      // console.log(password, user);
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+      const token = createToken(user._id);
+      console.log("datatoken", token);
+      res.status(200).json({ user, token });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    const user = await db.findOne({ email });
-    console.log(user);
-    if (!user) {
-      return res.status(400).json({ message: "User does not exist" });
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-    const token = createToken(user._id);
-    console.log("datatoken", token);
-    res.status(200).json({ user, token });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-
-const forgotPassword = async (req, res) => {
-  const { email } = req.body;
-  const user = await db.findOne({ email });
-  if (!user) throw new Error("User not found with this email");
-  try {
-    const token = await user.createPasswordResetToken();
-    await user.save();
-    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</>`;
-    const data = {
-      to: email,
-      text: "Hey User",
-      subject: "Forgot Password Link",
-      htm: resetURL,
-    };
-    sendEmail(data);
-    res.json(token);
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+  };
+  
 
 
 
-
-
-module.exports = { registerUser, loginUser, forgotPassword };
+module.exports = { registerUser , loginUser};
