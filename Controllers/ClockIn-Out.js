f836b68404eb92373779f87a33e4d0458bfdd87b
Rehan-Multani <135141111+Rehan-Multani@users.noreply.github.com>
2023-12-26 22:04:53 +0530

    hy
    hyh

--- Controllers/ClockIn-Out.js
+++ Controllers/ClockIn-Out.js
@@ -7,41 +7,40 @@ const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${enco
     address
 )}&key=${apiKey}`;
 
- const getlocation=(req,res)=>
- {
+const getlocation = (req, res) => {
     fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-        if (data.status === 'OK') {
-            const location = data.results[0].geometry.location;
-            console.log('Latitude:', location.lat);
-            console.log('Longitude:', location.lng);
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.status === 'OK') {
+                const location = data.results[0].geometry.location;
+                console.log('Latitude:', location.lat);
+                console.log('Longitude:', location.lng);
 
-            const latitude = location.lat;
-            const longitude = location.lng;
+                const latitude = location.lat;
+                const longitude = location.lng;
 
-            axios.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`)
-                .then(response => {
-                    const locationData = response.data;
-                    console.log(locationData);
-                    // Process locationData here
-                    res.send(locationData); // Send location data as response to the client
-                })
-                .catch(error => {
-                    console.log('Error fetching location:', error);
-                    res.status(500).send('Error fetching location');
-                });
-        } else {
-            console.log('Error:', data.status);
-            res.status(500).send('Error fetching address');
-        }
-    })
-    .catch((error) => {
-        console.error('Error fetching data:', error);
-        res.status(500).send('Error fetching data');
-    });
+                axios.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`)
+                    .then(response => {
+                        const locationData = response.data;
+                        console.log(locationData);
+                        // Process locationData here
+                        res.send(locationData); // Send location data as response to the client
+                    })
+                    .catch(error => {
+                        console.log('Error fetching location:', error);
+                        res.status(500).send('Error fetching location');
+                    });
+            } else {
+                console.log('Error:', data.status);
+                res.status(500).send('Error fetching address');
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+            res.status(500).send('Error fetching data');
+        });
 
 
- }
+}
 
- module.exports={getlocation}
+module.exports = { getlocation }
