5b35185be48edc46aab6096e3cef8ae7b864434c
kuldeep571 <kuldeepvishwakarma3084@gmail.com>
2023-12-26 04:54:50 -0800

    dasd
    ed

--- Models/userModel/userModel.js
+++ Models/userModel/userModel.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
 
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
+    name : { type: String, required: true },
     email: { type: String ,required: true , unique: true},
-    gender: { type: String , required: true },
-    contact: { type: String , required: true },
     password: { type: String, required: true},
 },
     {
