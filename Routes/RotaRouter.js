2bad784c0c97ca48b52c7e3aadd7ceeed936f1d1
Ranjan Lamichhane <studylc29@gmail.com>
2024-01-09 19:16:17 +0545

    Finishing add rota

--- Routes/RotaRouter.js
+++ Routes/RotaRouter.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const { getData, postData, updateData, deleteData, filterData } = require("../Controllers/RotaController");
+const {
+  getData,
+  postData,
+  updateData,
+  deleteData,
+  filterData,
+} = require("../Controllers/RotaController");
 const router = express.Router();
-const { requireAuth } = require("../Middlewares/requireAuth")
+const { requireAuth } = require("../Middlewares/requireAuth");
 
-router.get("/", getData)
+router.get("/", getData);
 router.get("/filter", filterData);
-router.post("/", postData)
-router.put("/:id", updateData)
-router.delete("/:id", deleteData)
+router.post("/", postData);
+router.put("/:id", updateData);
+router.post("/delete/:id", deleteData);
 
 module.exports = router;
