da111c31d209a3faa7ccc9d973f0e897515d4856
Ranjan Lamichhane <studylc29@gmail.com>
2024-01-17 16:09:06 +0545

    salary sheet

--- Controllers/PayrollSalarySheetController.js
+++ Controllers/PayrollSalarySheetController.js
@@ -1,6 +1,61 @@
+const PayrollBonusSheetModel = require("../Models/PayrollBonusSheetModel");
 const PayrollHourlyModel = require("../Models/PayrollHourlyModel");
 const PayrollMonthlyModel = require("../Models/PayrollMonthlyModel");
 const db = require("../Models/PayrollMonthlyModel");
+const UserTimeRegistor = require("../Models/UserTimeRegistor");
+const userModel = require("../Models/userModel");
+
+function getDaysDifference(startDate, endDate) {
+  // Convert the dates to UTC to avoid issues with daylight saving time
+  const startUTC = Date.UTC(
+    startDate.getFullYear(),
+    startDate.getMonth(),
+    startDate.getDate()
+  );
+  const endUTC = Date.UTC(
+    endDate.getFullYear(),
+    endDate.getMonth(),
+    endDate.getDate()
+  );
+  console.log(startUTC, endUTC);
+  // Calculate the difference in milliseconds
+  const timeDifference = endUTC - startUTC;
+
+  // Convert the difference to days
+  const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  return daysDifference;
+}
+function getLastDayOrCurrentDate(year, month) {
+  const currentDate = new Date();
+  const inputDate = new Date(year, month - 1, 1); // month is zero-indexed
+  if (
+    currentDate.getFullYear() == year &&
+    currentDate.getMonth() == month - 1
+  ) {
+    return currentDate;
+  }
+
+  if (inputDate <= currentDate) {
+    // If the input month has already passed, return the last day of the previous month
+    const lastDayOfPreviousMonth = new Date(year, month, 0);
+    return lastDayOfPreviousMonth;
+  } else {
+    // If the input month is the current or future month, return the current date
+    return currentDate;
+  }
+}
+function getPreviousMonth(year, month) {
+  // Adjust the month and year for January
+  if (month === 0) {
+    month = 11;
+    year -= 1;
+  } else {
+    month -= 1;
+  }
+
+  return { year, month };
+}
 
 const getAllData = async (req, res) => {
   try {
@@ -18,6 +73,75 @@ const getAllData = async (req, res) => {
   }
 };
 
+const getSalarySheetForMonth = async (req, res) => {
+  const { date } = req.params;
+  const [year, month] = date.split("-");
+  const lastOrCurDay = getLastDayOrCurrentDate(year, month);
+  const users = await userModel
+    .find({ adminId: req.user?.id })
+    .populate("monthly_pay_grade");
+  const finalUsers = [];
+  async function processUsers() {
+    for (const user of users) {
+      const diff = getDaysDifference(new Date(user.joindate), lastOrCurDay);
+      console.log(diff);
+      const isValid =
+        new Date(user.joindate).getDate() <= lastOrCurDay.getDate();
+
+      if (diff >= 30 && isValid) {
+        const { month: prevMonth, year: prevYear } = getPreviousMonth(
+          lastOrCurDay.getFullYear(),
+          lastOrCurDay.getMonth()
+        );
+        const endDate = new Date(
+          lastOrCurDay.getFullYear(),
+          lastOrCurDay.getMonth(),
+          new Date(user.joindate).getDate()
+        );
+        const startDate = new Date(
+          prevYear,
+          prevMonth,
+          new Date(user.joindate).getDate()
+        );
+        const timeRegistor = await UserTimeRegistor.findOne({
+          userid: user._id,
+        });
+        let overtime = 0;
+        if (timeRegistor) {
+          timeRegistor?.clock?.forEach((clock) => {
+            if (
+              new Date(clock.clockouttime) >= startDate &&
+              new Date(clock.clockouttime) <= endDate
+            ) {
+              const totalHour = +clock.totaltime.split(":")[0];
+              if (totalHour > 8) {
+                overtime = overtime + (totalHour - 8);
+              }
+            }
+          });
+        }
+
+        const bonus = await PayrollBonusSheetModel.find({
+          userid: user._id,
+        }).populate("bonusid");
+        finalUsers.push({ user, bonus, overtime });
+      }
+    }
+  }
+  await processUsers();
+  // const filteredUsers = users.forEach(async (user) => {
+  //   const diff = getDaysDifference(new Date(user.joindate), lastOrCurDay);
+  //   console.log(diff);
+  //   const isValid = new Date(user.joindate).getDate() <= lastOrCurDay.getDate();
+  //   if (diff >= 30 && isValid) {
+  //     const bonus = await PayrollBonusSheetModel.find({ userid: user._id });
+  //     finalUsers.push({ ...user, bonus });
+  //   }
+  // });
+  res.status(200).json({ message: "Succesful", finalUsers });
+};
+
 module.exports = {
   getAllData,
+  getSalarySheetForMonth,
 };
--- Routes/PayrollSalarySheetRouter.js
+++ Routes/PayrollSalarySheetRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   getAllData,
+  getSalarySheetForMonth,
 } = require("../Controllers/PayrollSalarySheetController");
 const { requireAuth } = require("../Middlewares/requireAuth");
 const router = express.Router();
 
+router.get("/:date", requireAuth, getSalarySheetForMonth);
 router.get("/", getAllData);
 
 module.exports = router;
