f4b040f6c55cc342b3aa8d26ca25f65e10f4dbd5
Ranjan Lamichhane <studylc29@gmail.com>
2024-01-31 11:42:13 +0545

    fixing small bug

--- Routes/Rota/RotaRouter.js
+++ Routes/Rota/RotaRouter.js
@@ -12,11 +12,11 @@ const router = express.Router();
 const { requireAuth } = require("../../Middlewares/requireAuth");
 
 router.get("/", requireAuth, getData);
+router.get("/rota-single", requireAuth, getsingledata);
 router.get("/:date", requireAuth, checkRota);
 router.get("/filter", filterData);
 router.post("/", postData);
 router.put("/:id", updateData);
 router.post("/delete/:id", deleteData);
-router.get("/rota-single", requireAuth, getsingledata);
 
 module.exports = router;
