022b73006cc0ecf33cd47d0f4678af45374942c5
Ranjan Lamichhane <studylc29@gmail.com>
2024-02-06 18:51:06 +0545

    hello

--- Routes/LanguageRouter.js
+++ Routes/LanguageRouter.js
@@ -4,19 +4,23 @@ const fs = require("fs");
 
 router.put("/", (req, res) => {
   console.log(req.body);
-  fs.writeFile(
-    `your_json_file-${req.body.language}.json`,
-    JSON.stringify(req.body.data),
-    (err) => {
-      if (err) {
-        console.error("Error writing JSON file:", err);
-        res.status(500).json({ error: "Failed to update JSON file" });
-      } else {
-        console.log("JSON file updated successfully");
-        res.json({ message: "JSON file updated successfully" });
-      }
-    }
+  const currentDirectory = process.cwd();
+
+  // Construct the file path relative to the current working directory
+  const filePath = path.join(
+    currentDirectory,
+    "i18n",
+    `${req.body.language}.json`
   );
+  fs.writeFile(filePath, JSON.stringify(req.body.data), (err) => {
+    if (err) {
+      console.error("Error writing JSON file:", err);
+      res.status(500).json({ error: "Failed to update JSON file" });
+    } else {
+      console.log("JSON file updated successfully");
+      res.json({ message: "JSON file updated successfully" });
+    }
+  });
 });
 
 module.exports = router;
