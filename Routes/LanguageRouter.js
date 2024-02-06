86baffeddae91523ace3af1f478165be9ae47b19
Ranjan Lamichhane <studylc29@gmail.com>
2024-02-06 18:33:39 +0545

    req body update

--- Routes/LanguageRouter.js
+++ Routes/LanguageRouter.js
@@ -4,15 +4,19 @@ const fs = require("fs");
 
 router.put("/", (req, res) => {
   console.log(req.body);
-  fs.writeFile("your_json_file.json", JSON.stringify(req.body), (err) => {
-    if (err) {
-      console.error("Error writing JSON file:", err);
-      res.status(500).json({ error: "Failed to update JSON file" });
-    } else {
-      console.log("JSON file updated successfully");
-      res.json({ message: "JSON file updated successfully" });
+  fs.writeFile(
+    `your_json_file-${req.body.language}.json`,
+    JSON.stringify(req.body.data),
+    (err) => {
+      if (err) {
+        console.error("Error writing JSON file:", err);
+        res.status(500).json({ error: "Failed to update JSON file" });
+      } else {
+        console.log("JSON file updated successfully");
+        res.json({ message: "JSON file updated successfully" });
+      }
     }
-  });
+  );
 });
 
 module.exports = router;
