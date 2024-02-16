cb2ddb4b4f389d537d28bb4b83927678aceb90e6
Ranjan Lamichhane <studylc29@gmail.com>
2024-02-16 13:54:09 +0545

    project changed

--- Controllers/ProjectsController.js
+++ Controllers/ProjectsController.js
@@ -1,108 +1,118 @@
+const AdminModel = require("../Models/Admin/AdminModel");
 const db = require("../Models/ProjectsModel");
 
 const postData = async (req, res) => {
-    try {
-        const {
-            title,
-            cost,
-            startDate,
-            endDate,
-            priority,
-            assignedEmployees,
-            summary,
-            employeeId
-        } = req.body;
-        const ProjectData = await db.create({
-            title,
-            cost,
-            startDate,
-            endDate,
-            priority,
-            assignedEmployees: JSON.parse(assignedEmployees),
-            summary,
-            employeeId
-        });
+  try {
+    const { title, cost, startDate, endDate, priority, summary, employeeId } =
+      req.body;
+    const admin = await AdminModel.findById(req.user.id);
+    const ProjectData = await db.create({
+      title,
+      cost,
+      startDate,
+      endDate,
+      branch_id: admin.branch_id,
+      priority,
+      summary,
+      employeeId,
+    });
 
-        res.status(201).json({
-            success: true,
-            ProjectData,
-            message: " Projects Created successfully"
-        });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    res.status(201).json({
+      success: true,
+      ProjectData,
+      message: " Projects Created successfully",
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 const getAllData = async (req, res) => {
-    try {
-        const ProjectsAllData = await db.find();
-        res.status(200).json({ success: true, ProjectsAllData, message: "All Projects Data Fetched successfully" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const ProjectsAllData = await db.find();
+    res.status(200).json({
+      success: true,
+      ProjectsAllData,
+      message: "All Projects Data Fetched successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const getSingleData = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const ProjectsData = await db.findById(id);
+    const ProjectsData = await db.findById(id);
 
-        res.status(200).json({ success: true, ProjectsData, message: "Projects Single Data Fetched successfully" });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    res.status(200).json({
+      success: true,
+      ProjectsData,
+      message: "Projects Single Data Fetched successfully",
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
-
 const updateData = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const {
-            title,
-            cost,
-            startDate,
-            endDate,
-            priority,
-            assignedEmployees,
-            summary,
-            employeeId
-        } = req.body;
+  try {
+    const { id } = req.params;
+    const {
+      title,
+      cost,
+      startDate,
+      endDate,
+      priority,
+      assignedEmployees,
+      summary,
+      employeeId,
+    } = req.body;
 
-        const updatedData = await db.findByIdAndUpdate(id, {
-            title,
-            cost,
-            startDate,
-            endDate,
-            priority,
-            assignedEmployees: JSON.parse(assignedEmployees),
-            summary,
-            employeeId
-        }, { new: true });
+    const updatedData = await db.findByIdAndUpdate(
+      id,
+      {
+        title,
+        cost,
+        startDate,
+        endDate,
+        priority,
+        assignedEmployees: JSON.parse(assignedEmployees),
+        summary,
+        employeeId,
+      },
+      { new: true }
+    );
 
-        res.status(200).json({ success: true, updatedData, message: "Projects Updated successfully" });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    res.status(200).json({
+      success: true,
+      updatedData,
+      message: "Projects Updated successfully",
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 const deleteData = async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        await db.findByIdAndDelete(id);
+    await db.findByIdAndDelete(id);
 
-        res.status(200).json({ success: true, message: "Projects Removed successfully" });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    res
+      .status(200)
+      .json({ success: true, message: "Projects Removed successfully" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 module.exports = {
-    postData,
-    getAllData,
-    updateData,
-    deleteData,
-    getSingleData
+  postData,
+  getAllData,
+  updateData,
+  deleteData,
+  getSingleData,
 };
-
--- Models/ProjectsModel.js
+++ Models/ProjectsModel.js
@@ -1,40 +1,39 @@
 const mongoose = require("mongoose");
 
 const ProjectsSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-        },
-        cost: {
-            type: String,
-            required: true
-        },
-        startDate: {
-            type: String,
-            required: true,
-        },
-        endDate: {
-            type: String,
-            required: true,
-        },
-        priority: {
-            type: String,
-            required: true,
-        },
-        assignedEmployees: [{
-            type: String,
-            required: true,
-        }],
-        summary: {
-            type: String,
-            required: true,
-        },
-        employeeId: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "user"
-        }]
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    cost: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+    },
+    employeeId: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    branch_id: { type: mongoose.Schema.Types.ObjectId, ref: "branch" },
+  },
+  { timestamps: true }
 );
 module.exports = mongoose.model("projects", ProjectsSchema);
