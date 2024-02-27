ac53b126b3ff9a33958f5805832a1a9457d9f87b
pawaryas <yash.cs1993@mitindore.co.in>
2024-02-27 12:14:08 +0530

    changes...

--- Controllers/MachineSetupController.js
+++ Controllers/MachineSetupController.js
@@ -142,7 +142,7 @@ const connectMachine = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: error.message });
+    res.status(200).status(500).json({ message: error.message });
   }
 };
 const disconnectMachine = async (req, res) => {
